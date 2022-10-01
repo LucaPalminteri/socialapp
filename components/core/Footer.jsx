11c386c789c329c3bc925f572570124ce3714946
LucaPalminteri <lucapalminteri02@gmail.com>
2022-09-30 21:14:04 -0300

    feat: add reusable footer component

--- components/core/Footer.jsx
+++ components/core/Footer.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import Link from 'next/link'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
-function Footer() {
+function Footer({activeNow}) {
+
   return (
-    <div>
-        <Link href='/homepage'>
-            <button>Homepage</button>
-        </Link>
-        <Link href='/create'>
-            <button>Create</button>
-        </Link>
-        <Link href='/profile'>
-            <button>Profile</button>
-        </Link>
+    <div className='footer'>
+        <nav>
+            <Link href={'/homepage'}>
+              {
+                activeNow == 'HOMEPAGE' ?
+                <div className='line-icon'><HomeRoundedIcon className='footer-icon active' fontSize='large'  /></div> :
+                <HomeOutlinedIcon className='footer-icon' fontSize='large' /> 
+              }
+            </Link>
+            <Link href={'/create'}>
+              {
+                activeNow == 'CREATE' ?
+                <div className='line-icon'><AddCircleOutlinedIcon className='footer-icon active' fontSize='large' /></div> :
+                <AddCircleOutlineOutlinedIcon className='footer-icon' fontSize='large' /> 
+              }
+            </Link>
+            <Link href={'/profile'}>
+            {
+                activeNow == 'PROFILE' ?
+                <div className='line-icon'><AccountCircleRoundedIcon className='footer-icon active' fontSize='large' /></div> :
+                <AccountCircleOutlinedIcon className='footer-icon' fontSize='large' /> 
+              }
+            </Link>
+        </nav>
     </div>
   )
 }
