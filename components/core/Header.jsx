add91ac4f2740113ce38d760f1a190a478361048
LucaPalminteri <lucapalminteri02@gmail.com>
2022-09-30 21:58:00 -0300

    feat: add reusable Header component

--- components/core/Header.jsx
+++ components/core/Header.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { useState } from 'react';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Link from 'next/link';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import MarkChatUnreadOutlinedIcon from '@mui/icons-material/MarkChatUnreadOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
-function Header() {
+
+function Header({title}) {
+
+  const [isButtonToggled,setIsButtonToggled] = useState(false)
+
+  const style = {marginRight: '10px'}
+  const goBack = () =>{
+    setIsButtonToggled(prev => !prev)
+    window.history.back();
+  }
   return (
-    <div>Header</div>
+    <div className='header'>
+      <button onClick={goBack} className={isButtonToggled ? 'animation-btn' : 'normal-btn'}>
+        <ArrowBackIosNewIcon />
+      </button>
+      {
+        title == 'PROFILE' ? 
+        <Link href='/settings'>
+          <SettingsIcon fontSize='medium' style={style}/> 
+        </Link> 
+        :
+        title == 'HOMEPAGE' ? 
+        <ChatBubbleOutlineOutlinedIcon fontSize='medium' style={style}/>
+        :
+        title == 'CREATE' ? 
+        < SearchOutlinedIcon fontSize='large' style={style}/>
+        :
+        <h2>{title}</h2>
+      }
+    </div>
   )
 }
 
