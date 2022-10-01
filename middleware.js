91389e70527f9dc258f5b154efc814eb82c29999
LucaPalminteri <lucapalminteri02@gmail.com>
2022-09-30 21:13:21 -0300

    feat: finish routes logic beside login problem

--- middleware.js
+++ middleware.js
@@ -3,19 +3,36 @@ import { jwtVerify } from "jose";
 
 export async function middleware(request) {
 
-        const jwt = request.cookies.get("token");
-        if (jwt == undefined) return NextResponse.redirect(new URL("/", request.url));
+    const jwt = request.cookies.get("token");
+    
+    if (jwt == undefined) return NextResponse.redirect(new URL("/", request.url));
+    
+    // TODO: validate if the user is already logged in it shouldn't be able  to re log in
 
-        try {
-            const { payload } = await jwtVerify(jwt,new TextEncoder().encode(process.env.NEXT_PUBLIC_TOKEN_NAME));
-            console.log(payload);
-            return NextResponse.next();
-        } catch (error) {
-            console.log('e:' + error);
-            return NextResponse.next();
-        }
+    // if (jwt != undefined) {
+    //     console.log("undefindef bro");
+    //     if (request.nextUrl.pathname.includes("/login")) {
+    //         try {
+    //             await jwtVerify(jwt, new TextEncoder().encode(process.env.NEXT_PUBLIC_TOKEN_NAME));
+    //             return NextResponse.redirect(new URL("/homepage", request.url));
+    //         } catch (error) {
+    //             return NextResponse.next();
+    //         }
+    //     }
+    // }
+
+    console.log(request.nextUrl.pathname);
+    try {
+        const { payload } = await jwtVerify(jwt,new TextEncoder().encode(process.env.NEXT_PUBLIC_TOKEN_NAME));
+        console.log(payload);
+        return NextResponse.next();
+    } catch (error) {
+        console.log('e:' + error);
+        return NextResponse.next();
+    }
 }
 
+// it only compare the code above with the pages below
 export const config = {
     matcher: ['/homepage','/profile','/create','/settings']
 };
