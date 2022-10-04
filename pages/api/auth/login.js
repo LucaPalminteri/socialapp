f061cbaca4320ca32eb1bd79b269ca70d5eb184b
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-03 22:49:07 -0300

    fix: add login validation && delete userjs

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -5,31 +5,40 @@ import { supabase } from "../../../utils/supabaseClient";
 const loginHandler = (req,res) => {
     const { username, password } = req.body;
 
-    // TODO: validate entry data (user inputs)
-    
-    const token = sign(
-        {
-          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
-          username,
-          password
-        },
-        process.env.NEXT_PUBLIC_TOKEN_NAME
-      );
-
-    const serialized = serialize("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        path: "/",
-    });
-
-    res.setHeader("Set-Cookie", serialized);
-
-    return res.status(200).json({message: "Login successful", token, username, password});
-
-    // TODO: if something is wrong validate HTTP status errors
-    return res.status(401).json({ error: "Invalid credentials" });
+    async function fetchUsers() {
+      let isUser = false
+      let currentUser = {}
+      const { data } = await supabase.from("user").select();
+      isUser = data.some(user => {
+        if(user.username == username && user.password == password) {
+          currentUser = user
+          return true
+        }
+      })
+      if(isUser == false) return res.status(401).json({ error: "Invalid credentials" });
+      if(isUser == true) {
+        const token = sign(
+          {
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
+            ...currentUser, password:''
+          },
+          process.env.NEXT_PUBLIC_TOKEN_NAME
+        );
+  
+      const serialized = serialize("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict",
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+          path: "/",
+      });
+  
+      res.setHeader("Set-Cookie", serialized);
+  
+      return res.status(200).json({message: "Login successful", token, username, password});
+      }
+    }
+    fetchUsers()
 }
 
 export default loginHandler
--- utils/users.js
+++
@@ -1,8 +0,0 @@
-import { supabase } from "./supabaseClient";
-
-async function fetchUsers() {
-    const { data } = await supabase.from("account").select();
-    return data;
-}
-
-export default fetchUsers
