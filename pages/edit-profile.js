7c7d8aa5fc998afa142b22e72aefc9f693cae500
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-06 19:43:42 -0300

    feat: add edit-profile page

---
+++ pages/edit-profile.js
@@ -0,0 +1,13 @@
+import EditProfile from '../components/pages_components/EditProfile'
+import Header from '../components/core/Header'
+
+function editprofile() {
+  return (
+    <div>
+        <Header/>
+        <EditProfile/>
+    </div>
+  )
+}
+
+export default editprofile
