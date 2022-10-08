28acbbbedad6151b0a3519d16fff8c79ede62cfb
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-08 08:30:36 -0300

    create ideas endpoint api

--- pages/api/ideas.js
+++ pages/api/ideas.js
@@ -1,10 +1,18 @@
 import { supabase } from "../../utils/supabaseClient";
+import jwt from "jsonwebtoken"; 
 
 // [GET]
-export default function handleUsers(req, res) { 
+export default function handleUsers(req, res) {
+    const {token} = req.cookies 
+
+    const currentUsername = jwt.verify(token, process.env.NEXT_PUBLIC_TOKEN_NAME);
 
     const getUsers = async () => {
-        const { data } = await supabase.from("ideas").select();
+        const { data,error } = await supabase
+        .from("ideas")
+        .select()
+        .neq('username',currentUsername.username)
+        .order('created_at');
         return res.status(200).json(data)
     }
     getUsers()
