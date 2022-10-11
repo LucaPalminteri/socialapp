686a8c2abe1683cc845c39fb35a5afdae52aebca
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-11 18:40:57 -0300

    fix: change header logic

--- components/core/Header.jsx
+++ components/core/Header.jsx
@@ -4,47 +4,46 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import Link from 'next/link';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Avatar from '../core/Avatar'
 
 
-export default function Header({title,username}) {
+export default function Header({ title, username, showBackArrow }) {
 
   const [isButtonToggled,setIsButtonToggled] = useState(false)
 
-  const style = {marginRight: '10px'}
   const goBack = () =>{
     setIsButtonToggled(prev => !prev)
     window.history.back();
   }
   return (
     <div className='header'>
-      <div onClick={goBack} className={isButtonToggled ? 'animation-btn' : 'normal-btn'}>
-          <ArrowBackIosNewIcon />
+      {showBackArrow ? <div onClick={goBack} className={isButtonToggled ? 'header-left-icon animation-btn' : 'header-left-icon normal-btn'}>
+        <ArrowBackIosNewIcon />
+      </div> : <></>}
+      
+      { title === 'chat' ?
+        <div className='header-title'>
+          <Avatar size={30}/>
+          <h3 className='header-medium-icon'>{username}</h3>
         </div>
+        :
+          <h3 className='header-medium-icon'>{username}</h3>
+      }
+
       {
         title == 'PROFILE' ? 
-        <>
-        
-        <h3>{username}</h3>
         <Link href='/settings'>
-          <SettingsIcon fontSize='medium' style={style}/> 
+          <SettingsIcon fontSize='medium' className='header-right-icon' /> 
         </Link> 
-        </>
         :
         title == 'HOMEPAGE' ? 
-        <>
-        <div>{' '}</div>
         <Link href={'/chat'}>
-        <ChatBubbleOutlineOutlinedIcon fontSize='medium' style={style}/>
+          <ChatBubbleOutlineOutlinedIcon fontSize='medium' className='header-right-icon' />
         </Link>
-        </>
         :
         title == 'CREATE' ? 
-        <>
-        <div>{' '}</div>
-        < SearchOutlinedIcon fontSize='large' style={style}/>
-        </>
-        :
-        <h2>{title}</h2>
+        <SearchOutlinedIcon fontSize='large' className='header-right-icon' />
+        : <></>
       }
     </div>
   )
