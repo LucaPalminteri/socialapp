5ee19ca9c364fa51da6218b3bb61daa7f59864ed
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-13 20:49:46 -0300

    feat: create API that returns all the users the current user follows

---
+++ pages/api/actions/follow-list.js
@@ -0,0 +1,23 @@
+import { supabase } from "../../../utils/supabaseClient";
+import jwt from "jsonwebtoken"; 
+
+export default function handleUsers(req, res) { 
+    
+    const {token} = req.cookies
+  
+    if (token == "null") return res.status(401).json({ error: "Not logged in" });
+    const currentUser = jwt.verify(token, process.env.NEXT_PUBLIC_TOKEN_NAME);
+  
+    let followListArr = []
+    const getFollowList = async () => {
+        try {
+            const { data } = await supabase.from("user_follows").select().eq('user_id',currentUser.user_id);
+            data.map(user => followListArr.push(user.follow_user_id))
+            return res.status(200).json(followListArr)
+        }
+        catch (error) {
+            return res.status(401).json({message:"Error"})
+        }
+    }
+    getFollowList()
+}
