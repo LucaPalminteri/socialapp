d39683a541e38a23622a47f92331c42bd95f4bce
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-19 19:35:33 -0300

    feat: add notification circle that shows when the user have notifications

--- components/core/Footer.tsx
+++ components/core/Footer.tsx
@@ -11,20 +11,44 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { supabase } from '../../utils/supabaseClient';
 
 export default function Footer({activeNow}) {
 
-  const [user, setUser] = useState()
+  const [user, setUser] = useState({user_id:"",username:""})
+  const [showNotif, setShowNotif] = useState(false)
 
   useEffect(() => {
     getUser()
   }, [])
 
+  supabase
+  .channel('*')
+  .on('postgres_changes', { event: '*', schema: '*' }, payload => {
+    getNotifications()
+    console.log(payload.new.user_id_reciever == user.user_id)
+  })
+  .subscribe()
+
   const getUser = async () => {
     const {data} = await axios.get('/api/profile')
-    setUser(data.username)
+    setUser(data)
   }
-  
+
+  const getNotifications = async () => {
+    try {
+      const {data,error} = await supabase
+      .from("notifications")
+      .select()
+      .eq("to_user",user?.user_id)
+      .is("seen",null)
+
+      if(data?.length > 0) setShowNotif(true)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  if(user.user_id != "")getNotifications()
 
   return (
     <div className='footer'>
@@ -54,16 +78,17 @@ export default function Footer({activeNow}) {
             {
                 activeNow == 'NOTIFICATIONS' ?
                 <div className='line-icon'><NotificationsIcon className='footer-icon active' fontSize='large' /></div> :
-                <NotificationsNoneOutlinedIcon className='footer-icon' fontSize='large' /> 
+                <div className='notification-icon-container'><NotificationsNoneOutlinedIcon className='footer-icon' fontSize='large' /><div className={showNotif ? 'notification-circle' : ''}></div></div>
             }
             </Link>
-            <Link href={`/${user}`} >
+            <Link href={`/${user?.username}`} > 
             {
                 activeNow == 'PROFILE' ?
                 <div className='line-icon'><AccountCircleRoundedIcon className='footer-icon active' fontSize='large' /></div> :
                 <AccountCircleOutlinedIcon className='footer-icon' fontSize='large' /> 
             }
             </Link>
+            
         </nav>
     </div>
   )
