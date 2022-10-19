0ba94af18152a4a3dedaf6b1aed69e949597dd0d
LucaPalminteri <lucapalminteri02@gmail.com>
2022-10-19 19:36:15 -0300

    finish first version of notifications

--- components/pages_components/Notifications.jsx
+++ components/pages_components/Notifications.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 import NotificationList from "../core/NotificationList"
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { supabase } from '../../utils/supabaseClient';
+import {useEffect} from "next"
 
 
 function Notifications({notifications,currentUser}) {
+
+
+  const changeNotificationState = async () => {
+      try {
+        const {data,error} = await supabase
+        .from('notifications')
+        .select()
+        .is('seen',null)
+        .eq('to_user',currentUser.user_id)
+        
+        if(data.length > 0) {
+          try {
+            const {data,error} = await supabase
+            .from('notifications')
+            .update({
+              seen: true
+            })
+            .eq("to_user",currentUser.user_id)
+            .is("seen",null)
+            if (error) throw error
+          }catch(error) {
+            console.error(error);
+          }
+        }
+      }catch(error) {
+        console.error(error);
+      }
+  }
+  changeNotificationState()
 
     let arrNotifications = notifications.map((notif,index) => <NotificationList key={index} notif={notif} i={index}/>)
 
